88021acceca22fa97a727f267d3d0e55c2a125f7
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-27 09:13:33 +0900

    Chore - route 설정, header 컴포 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "./components/layouts/Layout";
+import { Header } from "./components/shared/header/Header";
 import { Login } from "./pages/auth/Login";
 import { Booking } from "./pages/booking/Booking";
 import { Main } from "./pages/main/Main";
+import { Mypage } from "./pages/mypage/Mypage";
 import { Timetable } from "./pages/timetable/Timetable";
 
 function App() {
   return (
-    <Layout>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Layout>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/booking" element={<Booking />} />
+          <Route path="/booking/*" element={<Booking />} />
           <Route path="/timetable" element={<Timetable />} />
+          <Route path="/mypage" element={<Mypage />} />
         </Routes>
-      </BrowserRouter>
-    </Layout>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
