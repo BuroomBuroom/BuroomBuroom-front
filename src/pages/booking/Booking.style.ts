a0aed2d8c71185b48aee1ec0ecfc8d5bece8b9d4
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-27 11:19:34 +0900

    Chore - 예약하기

---
+++ src/pages/booking/Booking.style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const BookingLayout = styled.section`
+  width: 100%;
+  height: calc(100% - 80px);
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`;
--- src/pages/booking/Booking.tsx
+++ src/pages/booking/Booking.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+import * as S from "./Booking.style";
+import { Title } from "../../components/shared/common/title/Title";
+import { Step } from "./step/Step";
+import { useRecoilState } from "recoil";
+import { bookingStep } from "../../atoms/booking";
 
 export const Booking = () => {
-  return <div>Booking</div>;
+  const [step, setStep] = useRecoilState(bookingStep);
+
+  return (
+    <>
+      <Title title="예약하기" />
+      <S.BookingLayout>
+        <Step step={step} />
+        <div>
+          <button onClick={() => setStep((prev) => prev + 1)}>plus</button>
+        </div>
+      </S.BookingLayout>
+    </>
+  );
 };
