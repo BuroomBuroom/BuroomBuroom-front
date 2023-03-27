a1c13d001950dd64520a060a4000843434a61ded
kimhyerin0909 <khyerin0909@gmail.com>
2023-03-27 11:19:04 +0900

    Style - height 100%

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -4,8 +4,9 @@ type Props = { children: React.ReactNode; bgColor?: string };
 
 export const Layout = ({ children, bgColor }: Props) => {
   return (
-    <div style={{ paddingTop: "70px", display: "flex", width: "100%", justifyContent: "center", backgroundColor: bgColor }}>
-      <div style={{ width: "80%" }}>{children}</div>
+    <div
+      style={{ paddingTop: "70px", display: "flex", width: "100%", height: "calc(100% - 70px)", justifyContent: "center", backgroundColor: bgColor }}>
+      <div style={{ width: "80%", height: "100%" }}>{children}</div>
     </div>
   );
 };
