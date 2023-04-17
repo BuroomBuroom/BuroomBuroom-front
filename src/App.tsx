4047045bd9d5ec3d488821b3def74f403f88c046
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-17 14:36:31 +0900

    Chore - query client

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,32 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { Layout } from "./components/layouts/Layout";
 import { Header } from "./components/shared/header/Header";
-import { Login } from "./pages/auth/Login";
 import { Booking } from "./pages/booking/Booking";
 import { Main } from "./pages/main/Main";
 import { Mypage } from "./pages/mypage/Mypage";
 import { Timetable } from "./pages/timetable/Timetable";
+import { Redirect } from "./pages/auth/Redirect";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Header />
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/booking" element={<Booking />} />
-            <Route path="/timetable" element={<Timetable />} />
-            <Route path="/mypage" element={<Mypage />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <BrowserRouter>
+          <Header />
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/oauth/callback" element={<Redirect />} />
+              <Route path="/booking" element={<Booking />} />
+              <Route path="/timetable" element={<Timetable />} />
+              <Route path="/mypage" element={<Mypage />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
