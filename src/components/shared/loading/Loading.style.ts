9a80986b1340959a44787148db8f03ce622f7d27
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-17 15:02:51 +0900

    Feat - loading component

---
+++ src/components/shared/loading/Loading.style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const LoadingBG = styled.div`
+  position: absolute;
+  display: flex;
+  margin-top: -70px;
+  margin-left: -10%;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoadingIFrame = styled.iframe`
+  width: 40%;
+  border: none;
+`;
---
+++ src/components/shared/loading/Loading.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./Loading.style";
+
+export const Loading = () => {
+  return (
+    <S.LoadingBG>
+      <S.LoadingIFrame src="https://lottie.host/?file=5721efd2-6108-4ef5-bf44-ae7876ed7bce/Jft3eZv9ea.json" />
+    </S.LoadingBG>
+  );
+};
