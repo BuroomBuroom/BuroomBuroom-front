6acae39631001e72622710345e2d48f4b6bb51ca
kimhyerin0909 <khyerin0909@gmail.com>
2023-04-26 20:28:47 +0900

    Add - 노선 정보

---
+++ src/constants/route.ts
@@ -0,0 +1,33 @@
+export const school = [35.18923752212406, 128.90371867907965];
+const daejue = [35.21330989661336, 128.96118039331807];
+const gunam = [35.19754568351485, 128.99525749024977];
+const sasang = [35.16235395357683, 128.9857225490315];
+const duckcheun = [35.210275545417225, 129.0065162889155];
+const minam = [35.20847794742647, 129.06938688241442];
+const dongrae = [35.20589072830812, 129.07864278407655];
+const seomyeon = [35.15886394604782, 129.06018449860778];
+const suyeong = [35.167468663277596, 129.11592036621218];
+const haeundae = [35.16372398590684, 129.15933380409228];
+
+export const route = [
+  [
+    { name: "대저역", fullName: "대저역 2번 출구", latlng: daejue, arrivedAt: "15:45" },
+    { name: "구남역", fullName: "구남역 3번 출구", latlng: gunam, arrivedAt: "15:55" },
+    { name: "사상역", fullName: "사상역 1번 출구", latlng: sasang, arrivedAt: "16:10" },
+  ],
+  [
+    { name: "대저역", fullName: "대저역 2번 출구", latlng: daejue, arrivedAt: "15:45" },
+    { name: "덕천역", fullName: "덕천역 9번 출구", latlng: duckcheun, arrivedAt: "16:00" },
+    { name: "미남역", fullName: "미남역 13번 출구", latlng: minam, arrivedAt: "16:15" },
+    { name: "동래역", fullName: "동래역 4번 출구", latlng: dongrae, arrivedAt: "16:20" },
+  ],
+  [
+    { name: "서면역", fullName: "서면역 12번 출구", latlng: seomyeon, arrivedAt: "16:05" },
+    { name: "수영역", fullName: "수영역 5번 출구", latlng: suyeong, arrivedAt: "16:20" },
+    { name: "해운대역", fullName: "해운대역 1번 출구", latlng: haeundae, arrivedAt: "18:35" },
+  ],
+  [
+    { name: "서면역", fullName: "서면역 12번 출구", latlng: seomyeon, arrivedAt: "16:05" },
+    { name: "동래역", fullName: "동래역 4번 출구", latlng: dongrae, arrivedAt: "16:20" },
+  ],
+];
