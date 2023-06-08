7d06f9592d87887907461b38acd5a21f67f7caf4
kimhyerin0909 <khyerin0909@gmail.com>
2023-06-08 10:03:18 +0900

    ADD :: 버스 시간표

--- src/pages/timetable/Timetable.tsx
+++ src/pages/timetable/Timetable.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { RouteMap } from "../../components/shared/routeMap/RouteMap";
 
 export const Timetable = () => {
-  return <div>Timetable</div>;
+  const [selected, setSelected] = useState<number>(1);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        marginTop: "50px",
+      }}>
+      <div
+        style={{
+          display: "flex",
+          width: "80%",
+          height: "100%",
+          gap: "2%",
+          marginBottom: "8%",
+        }}>
+        <RouteMap select={selected} setSelect={setSelected} idx={0} />
+        <RouteMap select={selected} setSelect={setSelected} idx={1} />
+      </div>
+      <div style={{ display: "flex", width: "80%", height: "100%", gap: "2%" }}>
+        <RouteMap select={selected} setSelect={setSelected} idx={2} />
+        <RouteMap select={selected} setSelect={setSelected} idx={3} />
+      </div>
+    </div>
+  );
 };
